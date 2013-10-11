5d4780eded2b5601be7f2db7b080343ca1464183
Michael Herman <michael@mherman.org>
2013-10-10 19:11:16 -0600

    added sorting

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(function() {
 
+	//** parse data */
 	function parseData(allData) {
 		var results = []
 		_.each((allData), function(element) {
@@ -10,29 +11,36 @@ $(function() {
 				race:element[3],
 				wins:element[4],
 				losses:element[5],
-				score:((element[4]/(element[5]+element[4]))) 
+				score:((element[4]/(element[5]+element[4])))
 			})
 		})
-		return (_.sortBy(results,function(x){return x.score})).reverse() 
+		return results
 	}
 
+	//** append data to html */
 	function appendHTML(array) {
-
-
 		var count = 1
 		for (var i = 0; i < data["data"].length; i++) {
-			$(".users").append('<tr><td>'+count+'</td><td>'+((array[i].score)* 100).toFixed(2)+'</td><td>'+array[i].username+'</td><td>'+array[i].fullName+'</td><td>'+array[i].region+'</td>'+
+			$(".users").append('<tr><td>'+((array[i].score)* 100).toFixed(2)+'</td><td>'+array[i].username+'</td><td>'+array[i].fullName+'</td><td>'+array[i].region+'</td>'+
 				'<td>'+array[i].race+'</td><td>'+array[i].wins+'</td><td>'+array[i].losses+'</td></tr>')
 			count++
 		}
 	}
 
-	
-	appendHTML(parseData(data["data"]))
+	//** sort the data via event handler*/
+	function attachEventHandlers(array) {
+		$('.sort').click(function(){
+			$('.users').empty()
+			var sort = ($(this).text().toLowerCase())
+			var test = (_.sortBy(array,function(x){return x[sort]})).reverse()
+			appendHTML(test)
+		})
+	}
 
+	//** run code */
+	var parsedData = parseData(data["data"])
+	attachEventHandlers(parsedData)
 
 })
-
-
 
 
